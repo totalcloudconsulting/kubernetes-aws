3f74427f8d4398e906cf2c1a6b99f200307e8f15
Tamas Tobi <tamas.tobi@gmail.com>
2018-03-07 09:33:47 +0100

    Delete option to S3 sync added

--- deploy-s3.sh
+++ deploy-s3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws s3 sync ./ s3://tc2-kubernetes/latest/ --profile=tc2-infra --exclude ".git/*" --exclude "$0"
+aws s3 sync ./ s3://tc2-kubernetes/latest/ --profile=tc2-infra --exclude ".git/*" --exclude "$0" --delete
